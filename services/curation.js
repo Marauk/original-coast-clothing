8a13904f8228a0c5eba44a355130d9961b16009f
marauk <maryia.aukhimovich@gmail.com>
2019-05-21 19:09:38 +0300

    trial2

--- services/curation.js
+++ services/curation.js
@@ -109,7 +109,7 @@ module.exports = class Curation {
       case "CURATION_FOR_ME":
       case "CURATION_GAMES":
       case "CURATION_SOMEONE_ELSE":
-        response = Response.genQuickReply(i18n.__("estimation"), [
+        response = Response.genQuickReply(i18n.__("estimation.c"), [
           {
             title: i18n.__("curation.work"),
             payload: "CURATION_OCASION_WORK"
